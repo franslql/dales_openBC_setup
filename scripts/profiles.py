f0c70e46b17ce20497c78cc705dc239a104bc5a9
Frans Liqui Lung <nmfl@ac6-102.bullx>
2023-06-12 23:07:10 +0000

    add functionality to create Nc_0 profile, can not be used in DALES yet

--- scripts/profiles.py
+++ scripts/profiles.py
@@ -22,7 +22,7 @@ def profiles(input,grid,initfields,data):
   vprof  = initfields['v0'].mean(dim=['xt','ym']).values
   e12prof = np.ones(np.shape(thlprof))*input['e12']
   # Write data
-  for i in range(input['kmax']):
+  for i in range(grid.kmax):
       prof.write(f"{grid.zt[i]} {thlprof[i]} {qtprof[i]} {uprof[i]} {vprof[i]} {e12prof[i]} \n")
       lscale.write(f"{grid.zt[i]} {0} {0} {0} {0} {0} {0} {0} \n")
       if(input['nsv']>0): scalar.write(f"{grid.zt[i]} " +" ".join(map(str,np.zeros(input['nsv'])))+" \n" )
@@ -36,4 +36,17 @@ def profiles(input,grid,initfields,data):
     for i in range(data.sizes['z']):
       exnr.write(f"{data['z'][i].values} {data['exnr'][i].values} \n")
     exnr.close()
+  # Create Nc0.inp.xxx
+  if('Nc_0' in input):
+    Nc_cst = input['Nc_0']['Nc_cst']
+    z_cst  = input['Nc_0']['z_cst']
+    z_e    = input['Nc_0']['z_e']
+    Nc0prof = np.ones(grid.kmax)*Nc_cst
+    Nc0prof[grid.zt>z_cst] = Nc_cst*np.exp(-(grid.zt[grid.zt>z_cst]-z_cst)/z_e)
+    fNc0 = open(f"{input['outpath']}nc0.inp.{input['iexpnr']:03}", 'w')
+    fNc0.write(f"# Cloud droplet number input profile created by {input['author']} at {time}\n")
+    fNc0.write('# z Nc_0\n')
+    for i in range(grid.kmax):
+      fNc0.write(f"{grid.zt[i]} {Nc0prof[i]} \n")
+    fNc0.close()
   return
