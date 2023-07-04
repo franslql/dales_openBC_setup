5b534b6090fca109e2b46808c95966a6b65b7bff
Frans Liqui Lung <nmfl@ac6-100.bullx>
2023-07-04 11:08:47 +0000

    added routines for fine simulation

--- scripts/create_input.py
+++ scripts/create_input.py
@@ -2,14 +2,15 @@
 import json
 from GridDales import GridDales
 from prep_harmonie import prep_harmonie
-from initial_fields import initial_fields
-from boundary_fields import boundary_fields
+from initial_fields import initial_fields, initial_fields_fine
+from boundary_fields import boundary_fields, boundary_fields_fine
 from profiles import profiles
-from surface_temperature import surface_temperature
+from surface_temperature import surface_temperature, surface_temperature_fine
 from synthetic_turbulence import synthetic_turbulence
 from gaussian_filter import gaussian_filter
+import sys
 #%% Read input file
-with open('input.json') as f: input = json.load(f)
+with open(sys.argv[1]) as f: input = json.load(f)
 #%% Create input for outer simulation
 if 'coarse' in input:
   input_coarse = input['coarse']
@@ -28,32 +29,39 @@ if 'coarse' in input:
   #%% Advective time interpolation of input data (optional, to be implemented)
   
   #%% Create initial fields > initfields.inp.xxx.nc
-  initfields = initial_fields(input_coarse,grid,data,transform)
-  print('finished initifields')
-  #%% Create profiles > prof.inp.xxx, lscale.inp.xxx scalar.inp.xxx
-  profiles(input_coarse,grid,initfields,data)
-  print('finished profiles')
+  if(input_coarse['start']==input_coarse['time0']): # Not required for warmstarts
+    initfields = initial_fields(input_coarse,grid,data,transform)
+    print('finished initial fields')
+    #%% Create profiles > prof.inp.xxx, lscale.inp.xxx scalar.inp.xxx
+    profiles(input_coarse,grid,initfields,data)
+    print('finished profiles')
   #%% Create boundary input > openboundaries.inp.xxx.nc
   openboundaries = boundary_fields(input_coarse,grid,data)
   print('finished boundary fields')
   #%% Create synthetic turbulence for boundary input (optional) > openboundaries.inp.xxx.nc
   if('synturb' in input_coarse): 
     synturb = synthetic_turbulence(input_coarse,grid,data,transform)
-    print('finished synturb')
+    print('finished synthetic turbulence')
   #%% Create heterogeneous and time dependend skin temperature > tskin.inp.xxx.nc (if ltskin==true)
   if('tskin' in input_coarse): 
     tskin = surface_temperature(input_coarse,grid,data,transform)
-    print('finished tskin')
+    print('finished surface temperature')
 
 #%% Write data to input files
 if('fine' in input):
   input_fine = input['fine']
   input_fine['author'] = input['author']
-  # Create DALES grid
-  grid = GridDales(input_fine)  
-  # Create initial fields > initfields.inp.xxx.nc
-
-  # Create boundary input > openboundaries.inp.xxx.nc
-
-  # Create heterogeneous and time dependend skin temperature > tskin.inp.xxx.nc
+  #%% Create DALES grid
+  grid = GridDales(input_fine['grid'])
+  #%% Create initial fields > initfields.inp.xxx.nc
+  if(input_fine['start']==input_fine['time0']): # Not required for warmstarts
+    initfields_fine = initial_fields_fine(input_fine,grid)
+    print('finished initial fields')
+  #%% Create boundary input > openboundaries.inp.xxx.nc
+  openboundaries_fine = boundary_fields_fine(input_fine,grid)
+  print('finished boundary fields')
+  #%% Create heterogeneous and time dependend skin temperature > tskin.inp.xxx.nc
+  if('tskin' in input_fine):
+    tskin_fine = surface_temperature_fine(input_fine,grid)
+    print('finished surface temperature')
 # %%
